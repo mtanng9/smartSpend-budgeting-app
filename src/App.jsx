abab6ba529edc20676708cd15846034230983a54
mtanng9 <mlt152498@gmail.com>
2024-11-13 22:46:50 -0500

    add basic home page styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,26 @@ function App() {
   return (
     <>
       <Header />
-      <LogIn />
+      <div className="flex h-screen ">
+        <div className="flex-col m-auto">
+          <h1 className="text-9xl text-bold">SMART SPEND</h1>
+          <h2 className="text-4xl text-center mb-10">
+            Track your income and expenses effortlessly
+          </h2>
+          <div className="justify-center">
+            <a
+              href="#"
+              className="text-2xl outline-1 outline-alpine-green outline p-10 m-5 rounded-lg bg-money-green"
+            >
+              Let's Budget
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <LogIn />
       <SignUp />
       <SetBudget />
-      <AddAnExpense />
+      <AddAnExpense /> */}
     </>
   );
 }
